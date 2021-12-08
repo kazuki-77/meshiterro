9bfb5c0ddbf11353821d5038d58451b7ec0661a5
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-08 15:17:28 +0000

    [finish] 4-3

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,36 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  #post_image_paramsではフォームで入力されたデータが
+  #投稿データとして許可されているパラメータかどうかをチェックしている
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path #投稿一覧画面へリダイレクト
+  end
+
+  def index
+    @post_images = PostImage.all
+    #タイムライン上に表示する投稿データを取得できるようになる。
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
+  private
+   def post_image_params
+     params.require(:post_image).permit(:shop_name, :image, :caption)
+   end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '76859996984627831ff67639fae4a08bf95522ee8ebfeedc5d3076efc06f9a7af26c37daeca91aec0b5e0d71c1efd0f84833692955cdb6ad6378a72ff4d0242a'
